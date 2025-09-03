15c9c26c8e0dd414e8db77f7875df747f4d20f6b
nannaglaw1 <nannathedanishgirl@gmail.com>
2025-09-03 13:49:29 +0200

    .

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ const sum = (sumArray) => {
 
 console.log(sum([1, 2, 3]));
 console.log(sum([10, 20, 30, 40]));
+
+
+let a = [1, 2, 3];
+let max = a[0];
+
+a.forEach(num => {
+    if (num > max) {
+        max = num;
+
+    }
+});
+
+console.log(max);
+
+const countVowels = (s) => {
+    const vowels = 'aeiouæøåÅ';
+    let count = 0;
+
+    for (const letters of s){
+        if (vowels.includes(letters)){
+            count++;
+
+        }
+    }
+
+    return count;
+};
+
+const s = 'Åge bøjede syv fine rør ud.';
+console.log(countVowels(s));
